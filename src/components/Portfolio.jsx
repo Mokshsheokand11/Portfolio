86a7437ea1bdf2e3b0b1daa9928700ad6cfdfe97
Mokshsheokand11 <mokshsheokand11@gmail.com>
2026-02-20 23:37:23 +0530

    website preview

--- src/components/Portfolio.jsx
+++ src/components/Portfolio.jsx
@@ -64,7 +64,7 @@ function Portfolio() {
 
                 <div className="px-6 py-4 flex justify-around">
                   <button className="bg-green-500 hover:bg-green-800 text-white font-semibold px-4 py-2 rounded">
-                    Video
+                    Preview
                   </button>
 
                   <button
