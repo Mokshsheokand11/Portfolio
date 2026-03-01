c09ca60400016ca807d737715d84691afd4a631b
Mokshsheokand11 <mokshsheokand11@gmail.com>
2026-03-01 23:57:51 +0530

    Checker

--- src/components/Portfolio.jsx
+++ src/components/Portfolio.jsx
@@ -91,6 +91,16 @@ function Portfolio() {
                         Preview
                       </button>
                     </a>
+                  ) : name === "Typescript" ? (
+                    <a
+                      href="https://jhota-checker.vercel.app/"
+                      target="_blank"
+                      rel="noopener noreferrer"
+                    >
+                      <button className="bg-green-500 hover:bg-green-800 text-white font-semibold px-4 py-2 rounded">
+                        Preview
+                      </button>
+                    </a>
                   ) : (
                     <button className="bg-green-500 hover:bg-green-800 text-white font-semibold px-4 py-2 rounded">
                       Preview
@@ -133,6 +143,16 @@ function Portfolio() {
                   ) : name === "Reactjs" ? (
                     <a
                       href="https://github.com/Mokshsheokand11/runhost"
+                      target="_blank"
+                      rel="noopener noreferrer"
+                    >
+                      <button className="bg-blue-500 hover:bg-blue-800 text-white font-semibold px-4 py-2 rounded">
+                        Source code
+                      </button>
+                    </a>
+                  ) : name === "Typescript" ? (
+                    <a
+                      href="https://github.com/Mokshsheokand11/JHOTA-checker"
                       target="_blank"
                       rel="noopener noreferrer"
                     >
